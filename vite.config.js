5955c9b371096bafb4381689d2a88d0dcc7995b8
Oumou Bathily <oumou.bathily@epitech.eu>
2026-04-07 11:14:15 +0200

    config github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/mon-portfolio/',
   plugins: [
     react(),
     tailwindcss(),
